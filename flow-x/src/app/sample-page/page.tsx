a5e1d7024d8ffc912f2d66c905ecb9309922e29a
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-04 16:39:47 +0530

    Create page.tsx

---
+++ flow-x/src/app/sample-page/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Page() {
+  return (
+   <>
+   <h1>sample page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem aliquam officia, autem repellendus facere quaerat architecto rerum quo. Voluptates fugiat, repudiandae doloribus exercitationem optio atque dolorem molestias error voluptas tenetur?</p>
+   </>
+  )
+}
