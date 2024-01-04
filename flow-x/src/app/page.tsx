28da1bdea5a3113b629aca36581d43df69188049
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-04 16:35:25 +0530

    Update page.tsx

--- flow-x/src/app/page.tsx
+++ flow-x/src/app/page.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <>
-   <div className='w-full h-screen bg-[#1D1D1D]'>
-    <Navbar/>
+   <div className='w-full h-screen'>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident corporis iste rem non distinctio praesentium, nam atque facere numquam accusamus a inventore laboriosam quod recusandae tempora fuga culpa, cum sed.
    </div>
    </>
   )
