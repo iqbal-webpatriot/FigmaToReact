bbb2d1322386a928a75d165f0cf110d2b180e9c1
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-04 16:08:21 +0530

    add custom font

--- flow-x/src/app/layout.tsx
+++ flow-x/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Sora } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'],variable:'--font-inter' });
+const sora = Sora({ subsets: ["latin"], weight: ["400"] ,variable:'--font-sora' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${sora.variable}`}>{children}</body>
     </html>
   )
 }
