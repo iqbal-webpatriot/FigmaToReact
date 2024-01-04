5b18dc5e9465ef75b049a6534d76bdb0c68b5705
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-04 16:35:59 +0530

    add active link class

--- flow-x/src/components/ui/NextLink.tsx
+++ flow-x/src/components/ui/NextLink.tsx
@@ -1,17 +1,20 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import { ReactElement } from "react";
 
-type NextLinkProps={
-    title:string | ReactElement;
-    href:string;
-    
-    
-}
+type NextLinkProps = {
+  title: string | ReactElement;
+  href: string;
+};
 
-export default function NextLink({title,href}:NextLinkProps) {
+export default function NextLink({ title, href }: NextLinkProps) {
+  const currentPath = usePathname();
+  const isActiveRoute = href === currentPath ? "text-active" : "";
   return (
-    <Link href={href}>
+    <Link href={href} className={isActiveRoute}>
       {title}
     </Link>
-  )
+  );
 }
