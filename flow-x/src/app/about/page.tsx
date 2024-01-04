ee2f1070875fee8b39a4d4ec84501adbcb5a85bf
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-04 11:00:21 +0530

    initial setup

---
+++ flow-x/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Page() {
+  return (
+   <>
+   <h1 className="text-center text-teal-400">About page</h1>
+   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus velit quas libero distinctio delectus consectetur veritatis dignissimos corrupti non? Facilis beatae maxime aperiam sint amet dicta tempora omnis doloribus! Alias.</p>
+   </>
+  )
+}
