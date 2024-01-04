1993d7e83b9fc3f19377e49dd35b971515b10eea
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-04 17:27:13 +0530

    Update page.tsx

--- flow-x/src/app/page.tsx
+++ flow-x/src/app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/layout/Hero";
 
 export default function Home() {
   return (
    <>
    <div className='w-full h-screen'>
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident corporis iste rem non distinctio praesentium, nam atque facere numquam accusamus a inventore laboriosam quod recusandae tempora fuga culpa, cum sed.
+    <Hero/>
    </div>
    </>
   )
