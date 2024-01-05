269b3323e23341025e67bdaf847564e28b8e99ad
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-05 18:50:55 +0530

    add social links prop comp

--- flow-x/src/components/ui/CardIcon.tsx
+++ flow-x/src/components/ui/CardIcon.tsx
@@ -1,18 +1,29 @@
-import { ReactElement } from "react";
-import { AbstractIcon1 } from "../../../public/icons/Icons";
+import { ReactElement, ReactNode } from "react";
+import { BehanceIcon, DribbleIcon, FacebookIcon, InstagramIcon, TwitterIcon } from "../../../public/icons/Icons";
 
 type CardIconProps = {
   title: string;
   content: string;
   icon: ReactElement;
+  socialLinks?:boolean;
 };
 
-export default function CardIcon({title,content,icon}:CardIconProps) {
+export default function CardIcon({title,content,icon,socialLinks}:CardIconProps) {
   return (
-    <div className="w-[20.56rem] h-[10rem] flex flex-col justify-start gap-5">
+    <div className="w-[20.56rem]  flex flex-col justify-start gap-5">
      {icon}
     <h6 className="text-[1.25rem] leading-[1.62rem] text-[#E7E7E7]">{title}</h6>
     <p className="text-[0.9rem] leading-[1.62rem] font-inter text-[#B0B0B0]">{content}</p>
+    {socialLinks && (
+         <div className="w-[13.68rem] flex justify-between items-start">
+        <DribbleIcon/>
+        <InstagramIcon/>
+        <BehanceIcon/>
+        <TwitterIcon/>
+        <FacebookIcon/>
+         </div>
+    )}
     </div>
+  
   );
 }
