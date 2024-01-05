04851b8893dd01c3cc3754920eb27d7c5d6c5354
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-05 19:24:09 +0530

    Update CardIcon.tsx

--- flow-x/src/components/ui/CardIcon.tsx
+++ flow-x/src/components/ui/CardIcon.tsx
@@ -1,29 +1,54 @@
 import { ReactElement, ReactNode } from "react";
-import { BehanceIcon, DribbleIcon, FacebookIcon, InstagramIcon, TwitterIcon } from "../../../public/icons/Icons";
+import {
+  BehanceIcon,
+  DribbleIcon,
+  FacebookIcon,
+  InstagramIcon,
+  TwitterIcon,
+} from "../../../public/icons/Icons";
+import Link from "next/link";
 
 type CardIconProps = {
   title: string;
   content: string;
   icon: ReactElement;
-  socialLinks?:boolean;
+  socialLinks?: boolean;
 };
 
-export default function CardIcon({title,content,icon,socialLinks}:CardIconProps) {
+export default function CardIcon({
+  title,
+  content,
+  icon,
+  socialLinks,
+}: CardIconProps) {
   return (
     <div className="w-[20.56rem]  flex flex-col justify-start gap-5">
-     {icon}
-    <h6 className="text-[1.25rem] leading-[1.62rem] text-[#E7E7E7]">{title}</h6>
-    <p className="text-[0.9rem] leading-[1.62rem] font-inter text-[#B0B0B0]">{content}</p>
-    {socialLinks && (
-         <div className="w-[13.68rem] flex justify-between items-start">
-        <DribbleIcon/>
-        <InstagramIcon/>
-        <BehanceIcon/>
-        <TwitterIcon/>
-        <FacebookIcon/>
-         </div>
-    )}
+      {icon}
+      <h6 className="text-[1.25rem] leading-[1.62rem] text-[#E7E7E7]">
+        {title}
+      </h6>
+      <p className="text-[0.9rem] leading-[1.62rem] font-inter text-[#B0B0B0]">
+        {content}
+      </p>
+      {socialLinks && (
+        <div className="w-[13.68rem] flex justify-between items-start">
+          <Link href={"#"}>
+            <DribbleIcon />
+          </Link>
+          <Link href={"#"}>
+            <InstagramIcon />
+          </Link>
+          <Link href={"#"}>
+            <BehanceIcon />
+          </Link>
+          <Link href={"#"}>
+            <TwitterIcon />
+          </Link>
+          <Link href={"#"}>
+            <FacebookIcon />
+          </Link>
+        </div>
+      )}
     </div>
-  
   );
 }
