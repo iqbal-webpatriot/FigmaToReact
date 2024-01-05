eedce6f6056bf86eb2f426a7531828034b378f12
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-05 10:38:03 +0530

    Update page.tsx

--- flow-x/src/app/page.tsx
+++ flow-x/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/layout/Hero";
+import Services from "@/components/layout/Services";
 
 export default function Home() {
   return (
    <>
    <div className='w-full h-screen'>
     <Hero/>
+    <Services/>
    </div>
    </>
   )
