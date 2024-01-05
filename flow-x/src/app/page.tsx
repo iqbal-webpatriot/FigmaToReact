ba3bd08e240f2d8d52f474bcec481a8d25d9ea7e
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-05 12:23:24 +0530

    Update page.tsx

--- flow-x/src/app/page.tsx
+++ flow-x/src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/layout/About";
 import Hero from "@/components/layout/Hero";
 import Projects from "@/components/layout/Projects";
 import Services from "@/components/layout/Services";
@@ -9,6 +10,7 @@ export default function Home() {
     <Hero/>
     <Services/>
     <Projects/>
+    <About/>
    </div>
    </>
   )
