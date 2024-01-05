61a856fb9de1eb1eb918c8d764423c14dd57d7cc
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-05 17:53:03 +0530

    Update About.tsx

--- flow-x/src/components/layout/About.tsx
+++ flow-x/src/components/layout/About.tsx
@@ -1,3 +1,5 @@
+import ButtonBlackMedium from "../ui/ButtonBlackMedium";
+
 export default function About() {
   return (
     <>
@@ -8,11 +10,17 @@ export default function About() {
           alt="work space"
         />
         <div className="w-[35.25rem] h-[22.18rem] flex flex-col justify-start gap-5">
-          <h2 className="text-[3rem] leading-[3.6rem] text-[#E7E7E7]">Your team of specialists</h2>
-          <p className="font-inter text-[1rem] leading-[1.6rem] text-[#B0B0B0]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.</p>
-          <button className="w-[11.62rem] h-[4.37rem] rounded-[3.12rem] text-[0.9rem] text-[#E7E7E7] bg-[#181818]">
-          More about us
-          </button>
+          <h2 className="text-[3rem] leading-[3.6rem] text-[#E7E7E7]">
+            Your team of specialists
+          </h2>
+          <p className="font-inter text-[1rem] leading-[1.6rem] text-[#B0B0B0]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat.
+          </p>
+
+          <ButtonBlackMedium>More about us</ButtonBlackMedium>
         </div>
       </div>
     </>
