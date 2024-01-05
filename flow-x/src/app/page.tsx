4e7dad276b771b0b4e25ca35f2f9d2386180d1e5
iqbal-webpatriot <135306903+iqbal-webpatriot@users.noreply.github.com>
2024-01-05 15:58:38 +0530

    Update page.tsx

--- flow-x/src/app/page.tsx
+++ flow-x/src/app/page.tsx
@@ -2,6 +2,7 @@ import About from "@/components/layout/About";
 import Hero from "@/components/layout/Hero";
 import Projects from "@/components/layout/Projects";
 import Services from "@/components/layout/Services";
+import WhyUs from "@/components/layout/WhyUs";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     <Services/>
     <Projects/>
     <About/>
+    <WhyUs/>
    </div>
    </>
   )
